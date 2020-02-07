0dc43bd5a2f087e7f7d84e28d8580a85a51612d3
DoanHieu_IQ <54270198+doanhieucodegym@users.noreply.github.com>
2020-02-07 09:02:51 +0700

    Login_security

---
+++ src/main/java/com/example/demo/service/UserDetailsServiceImpl.java
@@ -0,0 +1,46 @@
+package com.example.demo.service;
+
+import com.example.demo.dao.AppRoleDAO;
+import com.example.demo.dao.AppUserDAO;
+import com.example.demo.model.AppUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    private AppRoleDAO appRoleDAO;
+    @Autowired
+    private AppUserDAO appUserDAO;
+
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        AppUser appUser =this.appUserDAO.findUserAccount(userName);
+        if(appUser == null){
+            System.out.println("User not found!"+userName);
+            throw new UsernameNotFoundException("User"+userName+"was not found in the database");
+        }
+        System.out.println("Found User:"+appUser);
+        List<String> roleNames =this.appRoleDAO.getRoleName(appUser.getUserId());
+        List<GrantedAuthority> grantList =new ArrayList<GrantedAuthority>();
+        if(roleNames !=null){
+            for(String role: roleNames){
+                //Role_User, Role_admin,...
+                GrantedAuthority authority =new SimpleGrantedAuthority(role);
+                grantList.add(authority);
+            }
+        }
+        UserDetails userDetails = (UserDetails) new User(appUser.getUserName(), //
+                appUser.getEncrytedPassword(), grantList);
+        return null;
+    }
+}
